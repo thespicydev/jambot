378fa820bc0e97d0aae5649a621f144ee55cdacc
Tyler <tyler@thespicydev.com>
2019-12-18 20:31:53 -0500

    Add -l (--log) option to customize logging level

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ DISCORD_TOKEN=whatever_your_token_is
 from bots import dummy_bot, jam_bot
 from dotenv import load_dotenv
 
+import argparse
+import logging
 import os
 
 
@@ -19,4 +21,18 @@ def main():
     spicy_bot.run(token)
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-l",
+        "--log",
+        dest="log_level",
+        choices=[
+            'DEBUG','INFO', 'WARNING', 'ERROR', 'CRITICAL'
+        ],
+        default='INFO',
+        help="Set the logging level")
+
+    args = parser.parse_args()
+    if args.log_level:
+        logging.basicConfig(level=logging.getLevelName(args.log_level))
     main()
